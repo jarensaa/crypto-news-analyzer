2dd6e5547c71ed54b4be40c1e4a2f9cf1e4611ed
Mathias Van Herreweghe <mathias.van.herreweghe@hotmail.com>
2018-12-10 16:00:17 +0100

    implemented PELT

--- model_timeline.py
+++ model_timeline.py
@@ -4,6 +4,8 @@ from cryptoscraper import configure_db
 import pandas as pd 
 import matplotlib.pyplot as plt
 import datetime as dt
+import random
+import ruptures as rpt
 
 
 
@@ -54,19 +56,47 @@ def generate_timeseries(coin):
     timeseries = build_timeline(cursor)
     return timeseries
 
-def plot_price_volume(from, to, df):
-    fig, ax1 = plt.subplots(figsize=(15,6))
-    ax1.plot(timestamp_arr_sample, price_arr_sample, 'b-')
+def plot_price_volume(from_date, to_date, df):
+    price_arr = timeseries['price'].loc[from_date:to_date].values
+    vol_arr = timeseries['volume'].loc[from_date:to_date].values
+    timestamp_arr = timeseries.loc[from_date:to_date].index.values
+
+    _, ax1 = plt.subplots(figsize=(15,6))
+    ax1.plot(timestamp_arr, price_arr, 'b-')
     ax1.set_xlabel('timestamp')
     ax1.set_ylabel('price (high)', color='b')
     ax2 = ax1.twinx()
-    ax2.plot(timestamp_arr_sample, vol_arr_sample, 'g-')
+    ax2.plot(timestamp_arr, vol_arr, 'g-')
     ax2.set_ylabel('volume', color='g')
-    plt.xticks(random.sample(timestamp_arr_sample.tolist(), 10), rotation='vertical')
     plt.show()
+
+def plot_change_points(df, label, from_date, to_date):
+    signal = timeseries[label].loc[from_date:to_date].values
+    timeline = df.loc[from_date:to_date].index
+
+    algo = rpt.Pelt(model="rbf").fit(signal)
+    result = algo.predict(pen=10)
+
+    plt.plot(timeline, signal, 'b-')
+    for xc in np.take(timeline, [x - 1 for x in result]):
+        plt.axvline(x=xc, color='black', linestyle='--')
+
+    plt.show()
+
+def get_change_point_dates(df, label, from_date, to_date):
+    signal = timeseries[label].loc[from_date:to_date].values
+    timeline = df.loc[from_date:to_date].index
+
+    algo = rpt.Pelt(model="rbf").fit(signal)
+    result = algo.predict(pen=10)
+
+    return np.take(timeline, [x - 1 for x in result])
+
 # example call
 timeseries = generate_timeseries("BTC")
-print(timeseries)
-
-
-timeseries = generate_timeseries("BTC")
+# example plot of price and volume for given timespan
+plot_price_volume("20161202", "20161210", timeseries)
+# example plot of change points using PELT
+plot_change_points(timeseries, 'price', "20161202", "20161210")
+# example of retrieving change point dates
+cp_dates = get_change_point_dates(timeseries, 'price', "20161202", "20161210")
