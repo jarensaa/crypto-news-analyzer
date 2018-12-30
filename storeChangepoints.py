57a62decf726726c3108520cc19b9a0769e00b76
ulie <ulrike.schnaithmann@gmail.com>
2018-12-30 01:01:56 +0100

    added changepoint storage

--- storeChangepoints.py
+++ storeChangepoints.py
@@ -3,6 +3,7 @@ from cryptoApp.mongoService.queries import queryDatabase
 from cryptoApp.cryptoHandler.modelTimeline import get_change_point_dates
 from cryptoApp.cryptoHandler.modelTimeline import generate_whole_timeseries
 import pandas as pd
+import numpy as np
 
 def scan_for_change_points(coin, granularity=7, stride=3, dbgran=24):
     """ scans entire available time series of coin for change points 
@@ -18,11 +19,42 @@ def scan_for_change_points(coin, granularity=7, stride=3, dbgran=24):
     win = granularity * dbgran
     stride = stride * 24
     
-    for entry in range(len(timeseries['price'])):
+    for entry in range(0,len(timeseries['price']),stride):
         start = timeseries['time'].iloc[entry]
         end = timeseries['time'].iloc[win+entry]
-        t = get_change_point_dates(timeseries, 'price', start, end)
-    signal = timeseries['price']
+        changepoints = get_change_point_dates(timeseries, 'price', start, end)
+
+        start_unix = timeseries['unix_time'].iloc[entry]
+        end_unix = timeseries['unix_time'].iloc[win+entry]
+        store_changepoint(changepoints, start_unix, end_unix, coin)
+
+
     return timeseries
+
+
+def store_changepoint(cpoints, start, end, coin):
+    """ stores detected changepointss
+    :param changepoint: 
+    :param start: string of startdate, YYYYMMDD
+    :param end: string of enddate, YYYYMMD
+    :param coin: coin string, eg. 'BTC'
+    """
+    
+    client = getMongoClient()
+    collection = client.cryptoposts.changepoints
+
+    entries = []
+    for date in cpoints:
+        entry = {}
+        unix = int(date.value/10**9)
+        entry["changepoint"] = str(unix)
+        entry["start"] = str(start)
+        entry["end"] = str(end)
+        entry["coin"] = str(coin)
+        entries.append(entry)
+    
+    valid = collection.insert_many(entries)
+
+    client.close()
 
 t = scan_for_change_points('BTC')
